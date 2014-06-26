91de2799a9834b4f8d92c1b5ad852b5da562361c
Kim Janssens <kimjanssens21@gmail.com>
2014-06-26 14:45:20 +0200

    admin page created

--- scoreboard/routes/index.js
+++ scoreboard/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/admin', function(req, res) {
+  res.render('admin', {title: 'Admin page'});
+});
+
 module.exports = router;
